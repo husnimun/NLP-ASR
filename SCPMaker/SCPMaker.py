ee08b53d99e7b55faa7fd93a1a0d19076b863a4f
Rahman Adianto <rahman.adianto@gmail.com>
2016-12-12 16:10:04 +0700

    MFCC feature extraction

--- SCPMaker/SCPMaker.py
+++ SCPMaker/SCPMaker.py
@@ -4,11 +4,11 @@
 from os import listdir
 from os.path import isfile, join, splitext
 
-files = [f for f in listdir('.') if isfile(join('.', f))]
+files = [f for f in listdir('./recording') if isfile(join('./recording', f))]
 
 with open('data.scp', 'w') as scp_data:
     for file in files:
         filename, extension = splitext(file)
-        if extension != '.py':
-            scp_data.write('{0} {1}\n'.format(file, filename + '.mfc'))
+        if extension == '.wav':
+            scp_data.write('recording/{0} {1}\n'.format(file, 'mfcc/' + filename + '.mfc'))
             
