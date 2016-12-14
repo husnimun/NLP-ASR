9ffec81932506ae75e86d049682f47a921f93db0
Husni Munaya <husnimun@gmail.com>
2016-12-14 18:46:10 +0700

    Fix step2

--- step-2/WlistProcessor.py
+++ step-2/WlistProcessor.py
@@ -6,13 +6,14 @@ lines = []
 wlist = []
 transcript_prefix = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
 
-for  prefix in transcript_prefix:
+for prefix in transcript_prefix:
     lines += open('transcript/' + prefix + '-raw.tsv').read().splitlines()
 
+lines = [line.lower() for line in lines]
 
 for line in lines: 
     line_new = line.translate(None, string.punctuation)
-    transcript = line_new.split()[2]
+    transcript = line_new.split()[1]
     wlist += [w for w in transcript.split() if w not in wlist]
 
 wlist.sort()
